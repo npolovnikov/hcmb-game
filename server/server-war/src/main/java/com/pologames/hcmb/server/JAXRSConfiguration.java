bfd6ba3eb11f0de14698cf4ab90b2f029fdcde87
npolovnikov <npolovnikov@techinfocom.com>
2018-06-29 14:40:44 +0500

    Vaadin client

--- server/server-war/src/main/java/com/pologames/hcmb/server/JAXRSConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.pologames.hcmb.server;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-}
